d492bc2f1736fb56f6f0cf8b4edfd88e403f8c75
rexocean <15531124244@163.com>
2025-09-20 01:39:49 +0800

    hello world

---
+++ HelloWorld.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    constructor(){
+
+    }
+
+    function hello() public pure returns (string memory) {
+        return "Hello World!";
+    }
+}
